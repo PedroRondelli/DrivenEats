409ac3e79512639aae3e54e79a7a0b02da8d0aa1
Pedro Rondelli <pedrorondelli@hotmail.com>
2022-08-15 15:34:28 -0300

    Mecanismo de check

--- script.js
+++ script.js
@@ -19,5 +19,35 @@ function selecionarbebida(escolha){
     }
     escolhasobre.classList.toggle("sobremesaselecionada")
  }
+ function aparecericone(numeracaoicone){
+    const checado = document.querySelector(".check")
+    if(checado!== null){
+        checado.classList.remove("check")
+        checado.classList.add("vasco")
+    }
+    const checando = document.querySelector(numeracaoicone)
+    checando.classList.remove("vasco")
+    checando.classList.add("check")
+}
+function aparecericonebebida(numeracaobebida){
+    const checadobebida = document.querySelector(".checkbebida")
+    if(checadobebida!== null){
+        checadobebida.classList.remove("checkbebida")
+        checadobebida.classList.add("vasco")
+    }
+    const checkpresent = document.querySelector(numeracaobebida)
+    checkpresent.classList.remove("vasco")
+    checkpresent.classList.add("checkbebida")
+}
+function aparecericonesobremesa(numeracaosobremesa){
+    const checadosobremesa = document.querySelector(".checksobremesa")
+    if(checadosobremesa!== null){
+        checadosobremesa.classList.remove("checksobremesa")
+        checadosobremesa.classList.add("vasco")
+    }
+    const checkatual = document.querySelector(numeracaosobremesa)
+    checkatual.classList.remove("vasco")
+    checkatual.classList.add("checksobremesa")
 
+}
 
